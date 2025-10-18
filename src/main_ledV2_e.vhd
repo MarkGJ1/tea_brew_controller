b6ffb5c3b36ce90feeebde042afff8f970ae46ae
Marko Gjorgjievski <Gjorgjievski.m@protonmail.com>
2025-10-18 04:51:10 +0200

    Tackling issue: entity-arch-divide

--- src/main_ledV2_e.vhd
+++ src/main_ledV2_e.vhd
@@ -23,3 +23,34 @@ entity main_ledV2_e is
     ld8_o : out std_logic);
 
 end entity;
+
+architecture main_ledV2_a of main_ledV2_e is
+
+component hb_led_e is
+
+  generic (hb_halfperiod_g : integer := 6000000);
+  port(
+  cp_i : in std_logic;
+  rb_i : in std_logic;
+  d_o  : out std_logic);
+  
+end component;
+
+begin
+  
+  ld1_o <= not rb_i;
+  
+  ld2_comp: hb_led_e
+  port map(
+  cp_i => cp_i,
+  rb_i => rb_i,
+  d_o  => ld2_o);
+  
+  ld3_o <= m0_i;
+  ld4_o <= m1_i;
+  ld5_o <= t0_i;
+  ld6_o <= t1_i;
+  ld7_o <= tx_i;
+  ld8_o <= snd_i;
+  
+end architecture;
