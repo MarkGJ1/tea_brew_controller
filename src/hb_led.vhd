584a9df8a017e408e832bee6eb337a0efff13e80
Marko Gjorgjievski <Gjorgjievski.m@protonmail.com>
2025-10-21 23:47:30 +0200

    Refreshed hb_led with additional lessons.
    Added: generics,unsigned conversion for fewer LUTs, comments.

--- src/hb_led.vhd
+++ src/hb_led.vhd
@@ -1,49 +1,48 @@
+-- File name : hb_led.vhd
+-- Description : Heartbeat LED module
+-- Author : Marko Gjorgjievski
+-- Date created : 19.10.2025
+-- Date modified : /
+
 library IEEE;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
---
--- 12 000 000 / 2 = 6 000 000
---
+
 entity hb_led_e is
 
-  generic (
-  hb_halfperiod_g : integer := 6000000
-  );
-  
-  port(
-    cp_i : in std_logic;
-    rb_i : in std_logic;
-    d_o  : out std_logic);
-	
+	generic(
+		hb_halfperiod_g : natural := 13_500_000; -- generates 0.5s half period at 27MHz clock (Tang Nano 9k)
+		counter_width_g : natural := 24
+	);
+	port (
+		cp_i : in std_logic;
+		rb_i : in std_logic;
+		d_o : out std_logic);
+
 end entity;
 
-architecture hb_a of hb_led_e is
+architecture hb_led_a of hb_led_e is
 
-  signal time_s : integer range 0 to hb_halfperiod_g-1;
-  signal b_s    : std_logic;
+	signal counter : unsigned(counter_width_g-1 downto 0);
+	signal b_s : std_logic;
 
 begin
-  
-  timer: process (cp_i, rb_i)
-  begin
-  
-   if rb_i = '0' then
-  
-   b_s <= '0';
-   time_s <= 0;
-  
-   elsif rising_edge(cp_i) then
-   
-    if time_s < (hb_halfperiod_g-1) then 
-      time_s <= time_s + 1;
-    else
-      b_s <= not b_s;
-      time_s <= 0;
-    end if;
 
-   end if;
-  end process;
-  
-  d_o <= b_s;
-  
-end hb_a;
+	timer : process (cp_i, rb_i) -- heartbeat timer process
+	begin
+		if rb_i = '0' then -- asynchronous reset (active low)
+			b_s <= '0';
+			counter <= (others => '0');
+		elsif rising_edge(cp_i) then
+			if counter < to_unsigned(hb_halfperiod_g, counter'length) then
+				counter <= counter + 1; -- count upto half period
+			else
+				b_s <= not b_s; -- flip output
+				counter <= (others => '0');
+			end if;
+		end if;
+	end process;
+
+	d_o <= b_s; -- take internal register to output
+
+end hb_led_a;
---
+++ tb/hb_led_tb.vhd
@@ -0,0 +1,70 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity hb_led_tb is
+end entity;
+
+architecture hb_led_tb_a of hb_led_tb is
+
+    component hb_led_e is
+
+        generic(hb_halfperiod_g : natural := 500_000;
+                counter_width_g : natural := 19);
+        port (
+            cp_i : in std_logic;
+            rb_i : in std_logic;
+            d_o : out std_logic);
+
+    end component;
+
+    constant clk_freq_c : integer := 1_000_000; -- slower clock for faster simulation.
+    constant clk_period_c : time := 1000 ms/clk_freq_c;
+
+    signal cp_s : std_logic := '0';
+    signal rb_s : std_logic := '0';
+    signal d_s : std_logic;
+
+begin
+
+    dut : hb_led_e
+    port map(
+        cp_i => cp_s,
+        rb_i => rb_s,
+        d_o => d_s
+    );
+
+    cp_s <= not cp_s after clk_period_c / 2;
+
+    process is
+    begin
+
+        wait for 10 ms;
+        rb_s <= '1';
+
+        -- For ModelSIM no huge difference in simulation processing when it comes to 10ns to 1fs time resolution in real time margins (a few seconds).
+        -- For 1fs time resolution at 27MHz, transition is 4.238ns behind assertion time.
+        wait for 500 ms; -- Make sure time resolution is accurate enough, so that drift doesn't accumulate fast and assertion lands on right signal value.
+        assert (d_s = '0') report "Wrong output, LED still on" severity error;
+
+        wait for 1 ms;
+        assert (d_s = '1') report "Wrong output, LED still off" severity error;
+
+        -- Testing wait statement with until. Waits until event has occured.
+        wait until falling_edge(d_s); 
+        report "Hey." severity note;
+
+        -- Waits on signal change to do report.
+        wait on cp_s;
+        report "Hey again." severity note;
+
+        wait until falling_edge(d_s); 
+        report "Again." severity note;
+
+        -- Test end.
+        -- Make sure to enable simulation break on severity failure in ModelSIM to stop simulation.
+        report "Test Complete" severity failure;
+
+    end process;
+
+end architecture;
